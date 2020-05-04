39815f603393b2cc1cde1cda17a093c48917d7d2
John Munson <john.munson@monsanto.com>
2020-05-04 08:13:27 -0500

    add comments

--- scratch.elm
+++ scratch.elm
@@ -1,3 +1,21 @@
+{-
+If you want to run an elm file you have to provide the bare minium, you can't just
+do as in one line or one function. Here is a good example of such a bare minimum:
+https://ellie-app.com/7nVDGRBQr8ka1
+
+As you can see, a function was tested and without any model etc - the output fed to main.
+And if you don’t want to run it in elm repl or on elli you should consider that you expand
+your playground with and index.html in which you load your bare minimum elm js file, this
+way you also have the console logs.
+-}
+
+{-
+A main function is required, but can be reduced to `main = text ""`. Debug.log should be
+available unless you build for production with --optimize, as explained in its documentation.
+Alternatively, you could use ports for printing.
+-}
+
+
 -- Create a custom type
 type UserId = UserId String
 
